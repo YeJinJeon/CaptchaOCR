7b143cbe42b0a514a90a83c97aed35619192faa6
YeJinJeon <figure97@naver.com>
2021-04-26 18:05:42 +0900

    delete num_workers

--- train.py
+++ train.py
@@ -111,8 +111,8 @@ if __name__ == "__main__":
     train_dataset = CapchaDataset(train_data_path)
     val_dataset = CapchaDataset(val_data_path)
 
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=cpu_count, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=cpu_count, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     print(f'{len(train_loader)} batches in the train_loader')
     print(f'{len(val_loader)} batches in the test_loader')
