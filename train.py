7538954d1c3557a2869358428b0727139312f52e
YeJinJeon <figure97@naver.com>
2021-04-27 17:40:23 +0900

    fix print

--- train.py
+++ train.py
@@ -150,9 +150,11 @@ if __name__ == "__main__":
         start_epoch = 1
     
     if torch.cuda.device_count() > 1:
-        print("Let’s use ", torch.cuda.device_count(), 'GPUS!')
+        # print("Let’s use ", torch.cuda.device_count(), 'GPUS!')
         model = nn.DataParallel(model, device_ids=[0,1]).cuda()
-    # model = model.to(DEVICE)
+    else:
+        model = model.to(DEVICE)
+
 
     criterion = nn.CTCLoss(blank=0)
 
