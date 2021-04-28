4e726f5abfd05e483274baff5c5614f0655ed092
YeJinJeon <figure97@naver.com>
2021-04-28 11:38:32 +0900

    delete setting gpu1

--- train.py
+++ train.py
@@ -20,9 +20,6 @@ from config import *
 from model import CRNN
 from utils.util import *
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 cpu_count = mp.cpu_count()
 
